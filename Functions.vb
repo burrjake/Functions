915e9899a1612352c62197b7878bf771e21e7ec4
jakeb11499@gmail.com <jakeb11499@gmail.com>
2020-09-08 08:50:56 -0600

    Add project files.

---
+++ Functions.vb
@@ -0,0 +1,22 @@
+﻿Module Functions
+
+    Sub Main()
+        Dim aLetter As String
+        aLetter = "Q"
+        Console.WriteLine(aLetter)
+        Console.ReadLine()
+    End Sub
+
+    Function Letter()
+        Dim someOtherLetter As String
+        someOtherLetter = "Y"
+        Return someOtherLetter
+    End Function
+
+    Sub DoesNotReturn()
+        Dim someOtherLetter As String
+        someOtherLetter = "X"
+    End Sub
+
+    Function AddNumber() As Integer
+End Module
